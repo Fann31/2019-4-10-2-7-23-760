e30a8fd54c8d1f7f92804a2b9b6d41c390b4b740
fann <2211256376@qq.com>
2019-05-29 23:14:08 +0800

    计程车金钱问题

--- main/main.js
+++ main/main.js
@@ -1,4 +1,12 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+    var {distance,parkTime} = inputs;
+
+    let price = parkTime*0.25;
+    if(distance > 8){
+        price += (distance-8) * 1.2;
+        distance = 8;
+    }
+    price += ((distance -= 2) > 0) ? (distance * 0.8) +6 : 6;
+    price = (price%1 >= 0.5) ? Math.ceil(price) : Math.floor(price);
+    return price;
 };
